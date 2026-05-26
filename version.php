17f80f495fd146993c86b6efd1757e895a58a529
Yevhen Matasar <matasar.ei@gmail.com>
2026-05-26 22:25:35 +0300

    Refine CI pipeline to target PHP 7.4, MariaDB, and Moodle 4.1 LTS

--- version.php
+++ version.php
@@ -27,8 +27,8 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version    = 2024080300; // This component release level.
 $plugin->maturity   = MATURITY_STABLE; // This version's maturity level.
 $plugin->release    = 'v4.3.4'; // This version's release version.
-$plugin->requires   = 2022041200; // This version's moodle require release.
+$plugin->requires   = 2022112800; // This version's moodle require release.
 $plugin->component  = 'theme_academi'; // This component type_name.
 $plugin->dependencies = [
-    'theme_boost'  => 2022041900, // This version depended the component and its require release.
+    'theme_boost'  => 2022112800, // This version depended the component and its require release.
 ];
